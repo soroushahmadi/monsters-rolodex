a9adb7b40a59b32e0ce86339fa5d2c4f5ca4c15c
soroush <ahmadisorosh1@gmail.com>
2019-07-12 01:41:48 +0430

    Updates

---
+++ precache-manifest.7b4313fa8b0b43fef4982a79a1ae8533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5acf16cf117db3a0632c95c3bb7b09",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0860ce46f92ba0b0197e",
+    "url": "/monsters-rolodex/static/css/main.453e6183.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "0860ce46f92ba0b0197e",
+    "url": "/monsters-rolodex/static/js/main.3d5dad42.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
